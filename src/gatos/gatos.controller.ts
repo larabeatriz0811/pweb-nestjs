f0dffbb9bf9551bcc27ea296d82a4f664e1dfc1f
Glauber Ferreira <glauber.ferreira@nees.ufal.br>
2024-02-27 14:01:26 -0300

    cria a rota GET /gatos

--- src/gatos/gatos.controller.ts
+++ src/gatos/gatos.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('gatos')
-export class GatosController {}
+export class GatosController {
+
+    @Get()
+    findAll(): string {
+        return "Retorna a lista de todos os gatos."
+    }
+
+}
