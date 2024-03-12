1b46c81291b4b680dfb50b9658aa433454967f9a
larabeatriz0811 <laragattinha7@gmail.com>
2024-03-12 14:13:23 -0300

    nest g s gatos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GatosController } from './gatos/gatos.controller';
+import { GatosService } from './gatos/gatos.service';
 
 @Module({
   imports: [],
   controllers: [AppController, GatosController],
-  providers: [AppService],
+  providers: [AppService, GatosService],
 })
 export class AppModule {}
