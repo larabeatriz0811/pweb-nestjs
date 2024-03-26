eb95057f9ae8a43aa26f6e9b59cad4d8ea2f5076
larabeatriz0811 <laragattinha7@gmail.com>
2024-03-26 16:18:17 -0300

    configura acesso ao BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GatosController } from './gatos/gatos.controller';
 import { GatosService } from './gatos/gatos.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'dpg-co1gfpq1hbls73a3f25g-a.oregon-postgres.render.com',
+      port: 5432,
+      username: 'banco_teste_8urb_user',
+      password: '31/03',
+      database: 'banco_teste_8urb',
+      entities: [],
+      synchronize: true,
+      ssl: true,
+    }),
+  ],
   controllers: [AppController, GatosController],
   providers: [AppService, GatosService],
 })
 export class AppModule {}
+
+
